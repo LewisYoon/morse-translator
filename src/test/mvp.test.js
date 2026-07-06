61313662648c1ff6df933003f1f633dad9c3d5a6
LewisYoon <trexhoon1212@gmail.com>
2026-07-06 14:45:49 +1000

    adds all mvp test cases

--- src/test/mvp.test.js
+++ src/test/mvp.test.js
@@ -1,5 +1,66 @@
-import { engToMorse } from "../utils/translate.js";
+import { engToMorse, morseToEng, translate } from "../utils/translate.js";
 
-test("dummy test", () => {
-  expect(engToMorse("A")).toBe(".-");
+describe("MVP - english to morse && morse to english", () => {
+  // Eng to Morse
+
+  test("engToMorse - single letter", () => {
+    expect(engToMorse("A")).toBe(".-");
+  });
+
+  test("engToMorse - Multiple letter", () => {
+    expect(engToMorse("AB")).toBe(".- -...");
+  });
+
+  test("engToMorse - multiple words", () => {
+    expect(engToMorse("hello world")).toBe(
+      ".... . .-.. .-.. --- / .-- --- .-. .-.. -..",
+    );
+  });
+
+  test("engToMorse - single word", () => {
+    expect(engToMorse("hello")).toBe(".... . .-.. .-.. ---");
+  });
+
+  test("engToMorse - multiple words", () => {
+    expect(engToMorse("hello world")).toBe(
+      ".... . .-.. .-.. --- / .-- --- .-. .-.. -..",
+    );
+  });
+
+  test("engToMorse - to uppercase", () => {
+    expect(engToMorse("heLlO")).toBe(".... . .-.. .-.. ---");
+  });
+
+  // Morse to Eng
+  test("morseToEng - single morse code (letter)", () => {
+    expect(morseToEng(".-")).toBe("A");
+  });
+
+  test("morseToEng - multiple morse code (letter)", () => {
+    expect(morseToEng(".- -...")).toBe("AB");
+  });
+
+  test("morseToEng - single morse code (word)", () => {
+    expect(morseToEng(".... . .-.. .-.. ---")).toBe("HELLO");
+  });
+
+  test("morseToEng - multiple morse code (words)", () => {
+    expect(morseToEng(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")).toBe(
+      "HELLO WORLD",
+    );
+  });
+
+  test("morseToEng - check word separation using / ", () => {
+    expect(morseToEng(".... . / .-.. ---")).toBe("HE LO");
+  });
+
+  // Bonus MVP (auto detect input)
+
+  test("translate - detects English input", () => {
+    expect(translate("he")).toBe(".... .");
+  });
+
+  test("translate - detects Morse input", () => {
+    expect(translate(".... .")).toBe("HE");
+  });
 });
